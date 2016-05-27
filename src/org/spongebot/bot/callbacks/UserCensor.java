3b7cb124bc3cbe063f89dcade6b413bcaf98c2a5
juronen <juronen@users.noreply.github.com>
2016-05-27 00:45:14 -0400

    Update UserCensor.java

--- src/org/spongebot/bot/callbacks/UserCensor.java
+++ src/org/spongebot/bot/callbacks/UserCensor.java
@@ -14,7 +14,7 @@ public class UserCensor {
         } catch (Exception e) {
             e.printStackTrace();
             // This should never get thrown as the local player will only be null if you're logged out,
-            // and if you're logged out then player names won't be used anywhere. Some shit might go down because of
+            // and if you're logged out then player names won't be used anywhere. Something might go down because of
             // lagg though, and I'd rather not have it explode because of that.
         }
         return o;
